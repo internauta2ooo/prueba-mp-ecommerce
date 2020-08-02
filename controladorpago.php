54039455cb48e9b79c02dde1d27ab17a0cb295bd
Eduardo <eduardo>
2020-08-02 08:57:54 -0500

    seguimos haciendo cambios

--- controladorpago.php
+++ controladorpago.php
@@ -5,12 +5,19 @@ class controladorpago
 
     public function procesarPago($title, $quantity, $unit_price, $img)
     {
-       
+
         require __DIR__ .  '/vendor/autoload.php';
         // Agrega credenciales
         MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
         // Crea un objeto de preferencia
         $preference = new MercadoPago\Preference();
+
+        $preference->payment_methods = array(
+            "excluded_payment_methods" => array(
+                array("id" => "amex")
+            ),
+            "installments" => 6
+        );
 
         $payer = new MercadoPago\Payer();
         $payer->name = "Lalo";
@@ -30,13 +37,12 @@ class controladorpago
 
         // Crea un ítem en la preferencia
         $item = new MercadoPago\Item();
-        $url ="https://eduardo735-mp-commerce-php.herokuapp.com";
+        $url = "https://eduardo735-mp-commerce-php.herokuapp.com";
         //$url = "http://eduardo.compracelulares.com";
         $item->title = $title;
         $item->id = "1234";
         $item->quantity = $quantity;
-        $item->description = "Table is made of heavy duty white plastic and is 96 inches wide and 29 inches tall";
-        $item->category_id = "home";
+        $item->description = "“Dispositivo móvil de Tienda e-commerce";
         $item->unit_price = $unit_price;
         $item->currency_id = "MXN";
         $preference->notification_url = "https://webhook.site/03ef117d-5477-422e-861a-a4b18a754c35";
@@ -47,15 +53,15 @@ class controladorpago
             "pending" => $url . "/pending.php"
         );
         $preference->auto_return = "approved";
-        $preference->email="gentlelif3@outlook.com";
-        $preference->external_reference="gentlelif3@outlook.com";
+        $preference->email = "gentlelif3@outlook.com";
+        $preference->external_reference = "gentlelif3@outlook.com";
         $preference->save();
 
 
 
         $checkout = $preference->id;
         // var_dump($preference->back_urls);
-      
+
         var_dump("Retornameros la respuesta");
         var_dump($preference->email);
         return $checkout;
